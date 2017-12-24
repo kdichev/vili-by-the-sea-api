97532ad3e5b4eea4a216fde626366ba79ea837a6
Konstantin-home <konstantin.dichev@gmail.com>
2017-12-24 01:17:10 +0100

    add capacity check

--- src/CapacityCheck.js
+++ src/CapacityCheck.js
@@ -7,17 +7,32 @@ const createRange = (start, end) => {
   return moment.range(start, end)
 }
 
-const isRangesOverlapping = (primary, secondary) => {
+const isOverlappingRanges = (primary, secondary) => {
   return primary.overlaps(secondary)
 }
-
 
 export default async ({ data }) => {
   const queryRange = createRange(data.a, data.b)
   const searchData =  await fetchSearchData()
+  const { allRooms, allReservations } = searchData
+
+  let reservedRoomIds = [];
+  allReservations.map(reservation => {
+    if (isOverlappingRanges(queryRange, createRange(reservation.arrivalDate, reservation.departureDate))) {
+      return reservedRoomIds.push(...reservation.rooms)
+    }
+  })
+
+  const filteredResults = allRooms.filter(item =>
+    reservedRoomIds
+      .map(filter => (filter.id === item.id))
+      .map(val => val ? false : true)
+      .reduce((acc, cum) => acc && cum, reservedRoomIds.length)
+  )
+
   return {
     data: {
-      availableRooms: searchData
+      availableRooms: reservedRoomIds.length > 0 ? filteredResults : allRooms
     }
   }
 }
--- src/api.js
+++ src/api.js
@@ -14,11 +14,6 @@ const searchQuery = `
     }
     allRooms {
       id
-      reservations {
-        id
-        arrivalDate
-        departureDate
-      }
     }
   }
 `
