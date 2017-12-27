95722ddbbfbc77dfa16ac89a4053cdb34b21cccd
Konstantin-home <konstantin.dichev@gmail.com>
2017-12-27 23:10:51 +0100

    refactor api

--- src/CapacityCheck.js
+++ src/CapacityCheck.js
@@ -1,6 +1,25 @@
 import { fetchSearchData } from './api'
 import { createRange, isOverlappingRanges } from './dateRangeHelpers'
 
+/**
+ * Handles capacity check search
+ * @param {object} data - object with date ranges
+ */
+
+export default async ({ data }) => {
+  const queryRange = createRange(data.a, data.b)
+  console.log(await fetchSearchData())
+  const { allRooms, allReservations } = await fetchSearchData()
+  const reservedRoomIds = getAllReservedRoomIds(queryRange, allReservations)
+  const filteredResults = filterResultsWithRoomIds(allRooms, reservedRoomIds)
+  return {
+    data: {
+      availableRooms: reservedRoomIds.length > 0 ? filteredResults : allRooms
+    }
+  }
+}
+
+// finds all reserved room ids by searching all reservations using query range
 const getAllReservedRoomIds = (queryRange, allReservations) => {
   let reservedRoomIds = []
   allReservations.map(({ arrivalDate, departureDate, rooms }) => {
@@ -12,6 +31,7 @@ const getAllReservedRoomIds = (queryRange, allReservations) => {
   return reservedRoomIds
 }
 
+// filters rooms with reserved room ids and returns available rooms
 const filterResultsWithRoomIds = (allRooms, reservedRoomIds) => {
   return allRooms.filter(item =>
     reservedRoomIds
@@ -19,16 +39,4 @@ const filterResultsWithRoomIds = (allRooms, reservedRoomIds) => {
       .map(val => val ? false : true)
       .reduce((acc, cum) => acc && cum, reservedRoomIds.length)
   )
-}
- 
-export default async ({ data }) => {
-  const queryRange = createRange(data.a, data.b)
-  const { allRooms, allReservations } = await fetchSearchData()
-  const reservedRoomIds = getAllReservedRoomIds(queryRange, allReservations)
-  const filteredResults = filterResultsWithRoomIds(allRooms, reservedRoomIds)
-  return {
-    data: {
-      availableRooms: reservedRoomIds.length > 0 ? filteredResults : allRooms
-    }
-  }
 }
--- src/api.js
+++ src/api.js
@@ -1,36 +1,37 @@
-const fetch = require('node-fetch')
+import { GraphQLClient } from 'graphql-request'
 
-const API_LINK = process.env.API_URL
+const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTM1MTQwNTAsImNsaWVudElkIjoiY2o5ZDBxN21tMHY3cjAxNDBzMGlydzEyZSIsInByb2plY3RJZCI6ImNqYmEydHhkNjJnNXAwMTgycGV6d3V5MXoiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqYmFyNnJnbjA1czUwMTgxc2x6dXFyd3IifQ.iU03bMVJpzmcrYdWIiVe7TRZQtuNdacULfFC91fluPA'
 
-const searchQuery = `
-  query {
-    allReservations {
-      id
-      arrivalDate
-      departureDate
-      rooms {
+const client = new GraphQLClient(process.env.API_URL, {
+  headers: {
+    Authorization: API_KEY,
+  }
+});
+
+export const fetchSearchData = () => {
+  return client.request(`
+    query {
+      allReservations {
         id
+        arrivalDate
+        departureDate
+        rooms {
+          id
+        }
+      }
+      allRooms {
+        id
+        roomId
+        floor
+        imgUrl
+        roomTypes {
+          beds
+          type
+          rates {
+            price
+          }
+        }
       }
     }
-    allRooms {
-      id
-    }
-  }
-`
-
-const options = {
-  method: 'post',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTM1MTQwNTAsImNsaWVudElkIjoiY2o5ZDBxN21tMHY3cjAxNDBzMGlydzEyZSIsInByb2plY3RJZCI6ImNqYmEydHhkNjJnNXAwMTgycGV6d3V5MXoiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqYmFyNnJnbjA1czUwMTgxc2x6dXFyd3IifQ.iU03bMVJpzmcrYdWIiVe7TRZQtuNdacULfFC91fluPA'
-  },
-  body: JSON.stringify({
-    query: searchQuery
-  })
-}
-
-export const fetchSearchData = async () => {
-  let response = await fetch(API_LINK, options);
-  let data = await response.json();
-  return data.data;
+  `)
 }
