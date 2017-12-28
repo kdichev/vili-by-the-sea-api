2415a21c64ab4e48d34140003521b2a9a5b0032b
Konstantin Dichev <konstantin.dichev@gmail.com>
2017-12-28 08:08:43 +0100

    add travis yml (#1)
    * add travis yml
    
    * add simple test
    
    * add test
    
    * add correct node script
    
    * add tests back
    
    * add api key env var

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
-const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTM1MTQwNTAsImNsaWVudElkIjoiY2o5ZDBxN21tMHY3cjAxNDBzMGlydzEyZSIsInByb2plY3RJZCI6ImNqYmEydHhkNjJnNXAwMTgycGV6d3V5MXoiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqYmFyNnJnbjA1czUwMTgxc2x6dXFyd3IifQ.iU03bMVJpzmcrYdWIiVe7TRZQtuNdacULfFC91fluPA'
+const API_KEY = process.env.API_KEY
 
 const client = new GraphQLClient(process.env.API_URL, {
   headers: {
